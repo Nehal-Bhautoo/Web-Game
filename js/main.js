e7aec37baaa78c7334c8a9dae9d1e1a68f8dfc60
Nehal Bhautoo <nehalb34@gmail.com>
2020-06-18 23:45:18 +0400

    Change object

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 let scene = new THREE.Scene();
+scene.background = new THREE.Color("#6d6d6d");
 let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 let renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -14,40 +15,42 @@ window.addEventListener('resize', function () {
 THREE.controls = new THREE.OrbitControls(camera, renderer.domElement);
 
 //load model
-let materialLoader = new THREE.MTLLoader();
-let loaders = new THREE.ObjectLoader();
 
-// using the loader in a call back to load the model which is json here
-loaders.load('assets/model/male.json', function (object) {
+let objectLoader = new THREE.OBJLoader();
+objectLoader.setPath('assets/model/Gargoyle/');
+objectLoader.load('Gargoyle_1.obj', function (object) {
     scene.add(object);
-
-    let light = new THREE.PointLight(0xFF0040, 1, 100);
-    light.position.set( 50, 50, 50 );
-    scene.add(light);
-});
-
-// load a resource
-
-materialLoader.setPath('assets/model/Gargoyle/');
-materialLoader.load('Gargoyle_1.mtl', function (materials) {
-    materials.preload();
-
-    let objectLoader = new THREE.OBJLoader();
-    objectLoader.setMaterials(materials);
-    objectLoader.setPath('assets/model/Gargoyle/');
-    objectLoader.load('Gargoyle_1.obj', function (object) {
-        scene.add(object);
-        object.position.y -= 70; //positioning the model in the scene
-        object.position.x += 10;
-        object.translateZ(10);
-    });
+    object.position.y -= 70; //positioning the model in the scene
+    object.position.x += 10;
+    object.translateZ(10);
 });
 
 camera.position.z = 3;
 
+let light1 = new THREE.PointLight(0xFF0040, 4, 50);
+scene.add(light1);
+
+let light2 = new THREE.PointLight(0xFF040FF, 3, 50);
+scene.add(light2);
+
+let light3 = new THREE.PointLight(0x80FF80, 3, 50);
+scene.add(light3);
+
 // logic
 let update = function () {
+    let time = Date.now() * 0.0005;
 
+    light1.position.x = Math.sin(time * 0.7) * 30;
+    light1.position.y = Math.cos(time * 0.7) * 30;
+    light1.position.z = Math.cos(time * 0.7) * 30;
+
+    light2.position.x = Math.cos(time * 0.5) * 40;
+    light2.position.y = Math.sin(time * 0.5) * 40;
+    light2.position.z = Math.sin(time * 0.5) * 40;
+
+    light3.position.x = Math.sin(time * 0.5) * 30;
+    light3.position.y = Math.cos(time * 0.5) * 30;
+    light3.position.z = Math.cos(time * 0.5) * 30;
 };
 
 // draw scene
